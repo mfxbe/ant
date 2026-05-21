a12e615ed1de1d6f36be466b6a43510fde74bd37
Manuel Ehrmanntraut <dev@mfxbe.de>
2026-05-21 21:47:59 +0200

    Update Android NDK paths in ldc2 configuration

--- ldc2.conf.ex
+++ ldc2.conf.ex
@@ -5,9 +5,9 @@
 {
     switches = [
         "-defaultlib=phobos2-ldc,druntime-ldc",
-        "-gcc=/home/manuel/Android/Sdk/ndk/30.0.14904198/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android31-clang",
+        "-gcc=ANDROID/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android31-clang",
     ];
     lib-dirs = [
-        "/home/manuel/Android/lib-android_aarch64",
+        "ANDROID/lib-android_aarch64",
     ];
 };
