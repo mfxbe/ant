39fc7a6cf5046014e41b045ac0e65e9cb1b2b144
Manuel Ehrmanntraut <dev@mfxbe.de>
2026-05-28 18:48:36 +0200

    Update app.d

--- app.d
+++ app.d
@@ -1,36 +1,25 @@
-import std.stdio;
-import std.array;
-
 import gio.types : ApplicationFlags;
 import gtk.application;
 import gtk.application_window;
 import gtk.label;
 
 class ExampleApp:Application{
-	this() {
-		super("de.mfxbe.ExampleApp", ApplicationFlags.DefaultFlags);
-		connectActivate(&onActivate);
-	}
+        this() {
+                super("de.mfxbe.GiDAndroidApp", ApplicationFlags.DefaultFlags);
+                connectActivate(&onActivate);
+        }
 
-	void onActivate(){
-		auto window = new ApplicationWindow(this);
-		window.setDefaultSize(200, 200);
-		auto label = new Label("Hello World");
-		window.setChild(label);
+        void onActivate(){
+                auto window = new ApplicationWindow(this);
+                window.setDefaultSize(200, 200);
+                auto label = new Label("Hello World");
+                window.setChild(label);
 
-		window.present();
-	}
+                window.present();
+        }
 }
 
-//void main(string[] args) {
-//	auto app = new ExampleApp;
-//	app.run(args);
-//}
-
-extern(C) int main(int argc, char** argv){
-	import gio.c.functions;
-	import gio.c.types;
-
-	ExampleApp app = new ExampleApp;
-	return g_application_run(cast(GApplication*) app._cPtr, argc, argv);
+void main(string[] args) {
+        auto app = new ExampleApp;
+        app.run(args);
 }
